63087f2fd66e247426133ebdf6cb67f0dc6872d8
ItuMilla <itumelenglephondo94@gmail.com>
2023-08-20 04:44:20 -0700

    update

--- main.h
+++ main.h
@@ -1,3 +1,4 @@
+#ifndef MAIN_H
 #define MAIN_H
 #include <stdarg.h>
 #include <stdio.h>
@@ -113,4 +114,4 @@ int is_digit(char);
 long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
 
-#endif
+#endif /* MAIN_H */
